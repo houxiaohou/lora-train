c081c81a8999310767540b0714a137a5d1805653
xiaohou <xiyang.hou@interval.im>
2024-03-19 11:40:19 +0800

    - first commit

--- data_caption.py
+++ data_caption.py
@@ -1,8 +1,12 @@
+import json
+
 import torch
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
 MODEL_ID = 'Salesforce/blip-image-captioning-large'
+
+print('start model load...')
 
 processor = BlipProcessor.from_pretrained(MODEL_ID, cache_dir='/workspace/model')
 model = BlipForConditionalGeneration.from_pretrained(
@@ -13,8 +17,15 @@ model = BlipForConditionalGeneration.from_pretrained(
 
 print('model loaded....')
 
-for i in range(1, 8):
-    raw_image = Image.open(f'./dataset/dazhi/{i}.jpg').convert('RGB')
-    inputs = processor(raw_image, 'a photography of', return_tensors="pt").to("cuda", torch.float16)
-    out = model.generate(**inputs, num_beams=4, max_length=256, min_length=24)
-    print(processor.decode(out[0], skip_special_tokens=True))
+results = []
+
+with open('./dataset/dazhi/metadata.jsonl', 'w') as outfile:
+    for i in range(1, 8):
+        raw_image = Image.open(f'./dataset/dazhi/{i}.jpg').convert('RGB')
+        inputs = processor(raw_image, 'a photography of', return_tensors="pt").to("cuda", torch.float16)
+        out = model.generate(**inputs, num_beams=4, max_length=256, min_length=24)
+        caption = processor.decode(out[0])
+        result = {'file_name': f'{i}.jpg', 'text': caption}
+        json.dump(result, outfile)
+        outfile.write('\n')
+    outfile.close()
