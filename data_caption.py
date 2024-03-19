68e977d64c36c46ffc67c8637b08504bcae0e0f3
xiaohou <xiyang.hou@interval.im>
2024-03-19 16:11:00 +0800

    - first commit

--- data_caption.py
+++ data_caption.py
@@ -28,7 +28,7 @@ with open(META_PATH, 'w') as outfile:
         inputs = processor(raw_image, 'a photography of', return_tensors="pt").to("cuda", torch.float16)
         out = model.generate(**inputs, num_beams=4, max_length=256, min_length=24)
         caption = processor.decode(out[0], skip_special_tokens=True)
-        caption = caption.replace('a photography of', 'a TOK cat, ')
+        caption = caption.replace('a photography of', 'photo of a TOK cat')
         print(caption)
         result = {'file_name': f'{i}.jpg', 'text': caption}
         json.dump(result, outfile)
