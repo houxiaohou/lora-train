c0cbc29b9ae17dea2c690f4a47ecd5591ea811af
xiaohou <xiyang.hou@interval.im>
2024-03-19 12:12:40 +0800

    - first commit

--- data_caption.py
+++ data_caption.py
@@ -9,7 +9,7 @@ MODEL_ID = 'Salesforce/blip-image-captioning-large'
 DATA_PATH = './dataset/dazhi'
 META_PATH = './dataset/dazhi/metadata.jsonl'
 
-dataset = load_dataset('imagefolder', data_dir='./dataset/dazhi')
+dataset = load_dataset('imagefolder', data_dir='./dataset/dazhi', split='train')
 print(dataset)
 print(dataset[0])
 print(dataset[0]['text'])
